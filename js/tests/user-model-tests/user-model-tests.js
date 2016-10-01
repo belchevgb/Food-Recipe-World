b051c07efc48df39dd3d383a816b7acb44349f74
belchev.gb@gmail.com <belchev.gb@gmail.com>
2016-09-30 21:20:42 -0700

    User registration is tested.

--- js/tests/user-model-tests/user-model-tests.js
+++ js/tests/user-model-tests/user-model-tests.js
@@ -1,14 +1,24 @@
 describe('=== RECIPE MODEL ===', () => {
   describe('registerUser() tests', () => {
-    it('User should be registered with valid username and password.Object with username should be returned.', done => {
-      let requester = sinon.stub(app.requester, 'post', (url, headers, data) => {
+    let requester;
+
+    beforeEach(() => {
+      requester = sinon.stub(app.requester, 'post', (url, headers, data) => {
         return new Promise((resolve, reject) => {
-          if (data.username && data.password) {
+          if (Object.keys(data).length === 2 || (data.username && data.password)) {
             resolve({ username: data.username });
+          } else {
+            reject({
+              error: "Invalid credentials."
+            });
           }
         });
       });
+    });
 
+    afterEach(() => requester.restore())
+
+    it('User should be registered with valid username and password.Object with username should be returned.', done => {
       let userData = {
         username: 'username',
         password: '123456'
@@ -20,7 +30,19 @@ describe('=== RECIPE MODEL ===', () => {
           expect(response.username).to.equal(userData.username);
         })
         .then(done, done);
-      requester.restore();
+    });
+
+    it('User should NOT be registered if the data object has missing username or password', done => {
+      let userData = {};
+
+      app.userModel
+        .registerUser(userData)
+        .then(success => {
+          done(new Error());
+        }, error => {
+          console.log(error);
+          done();
+        });
     });
   });
 });
