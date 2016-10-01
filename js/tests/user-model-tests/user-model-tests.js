428dbbb59f31caaaa7b0b5c30664d75f71e2e69d
belchev.gb@gmail.com <belchev.gb@gmail.com>
2016-10-01 00:08:10 -0700

    Added tests for register, and loggin is tested.

--- js/tests/user-model-tests/user-model-tests.js
+++ js/tests/user-model-tests/user-model-tests.js
@@ -1,12 +1,14 @@
 describe('=== RECIPE MODEL ===', () => {
   describe('registerUser() tests', () => {
-    let requester;
+    let requester,
+      users = [];
 
     beforeEach(() => {
       requester = sinon.stub(app.requester, 'post', (url, headers, data) => {
         return new Promise((resolve, reject) => {
-          if (Object.keys(data).length === 2 || (data.username && data.password)) {
+          if (Object.keys(data).length === 2 && (data.username && data.password)) {
             resolve({ username: data.username });
+            users.push(data);
           } else {
             reject({
               error: "Invalid credentials."
@@ -16,7 +18,7 @@ describe('=== RECIPE MODEL ===', () => {
       });
     });
 
-    afterEach(() => requester.restore())
+    afterEach(() => app.requester.post.restore());
 
     it('User should be registered with valid username and password.Object with username should be returned.', done => {
       let userData = {
@@ -43,6 +45,109 @@ describe('=== RECIPE MODEL ===', () => {
           console.log(error);
           done();
         });
+    });
+
+    it('Expect app.requester.post() to be called once', done => {
+      let userData = {
+        username: 'username',
+        password: '123456'
+      };
+
+      app.userModel
+        .registerUser(userData)
+        .then(() => {
+          expect(app.requester.post.calledOnce).to.be.true;
+        })
+        .then(done, done);
+    });
+
+    it('Valid registration, should add the new user to all users', () => {
+      let userData = {
+        username: 'username',
+        password: '123456'
+      };
+
+      app.userModel
+        .registerUser(userData)
+        .then(() => {
+          expect(users.length).to.equal(1);
+        });
+    });
+  });
+
+  describe('loginUser() tests', () => {
+    const authToken = '6sadadsfb9xcv7vc6xv00rw0we7vds9',
+      users = [
+        {
+          username: 'username',
+          password: '123456'
+        }
+      ];
+
+    beforeEach(() => {
+      sinon.stub(app.requester, 'post', (url, headers, data) => {
+        return new Promise((resolve, reject) => {
+          let doesExist = users.some(u => u.username === data.username && u.password === data.password);
+
+          if (doesExist && Object.keys(data).length === 2 && data.username && data.password) {
+            resolve({
+              username: data.username,
+              id: 213213215321,
+              authToken
+            });
+          } else if (!doesExist) {
+            reject({
+              error: 'User not found.'
+            });
+          } else {
+            reject({
+              error: 'Invalid credentials.'
+            });
+          }
+        });
+      });
+    });
+
+    afterEach(() => {
+      localStorage.clear();
+      app.requester.post.restore();
+    });
+
+    it('Login with valid data, should return object with id, authToken and username', done => {
+      let userData = {
+        username: 'username',
+        password: '123456'
+      };
+
+      app.userModel
+        .loginUser(userData)
+        .then(response => {
+          expect(response.username).to.equal(userData.username);
+          expect(response.id).to.exist;
+          expect(response.authToken).to.exist;
+        })
+        .then(done, done);
+    });
+
+    it('Login with non existing user, should cause error', () => {
+      let userData = {
+        username: 'dsadsa',
+        password: 'password'
+      };
+
+      app.userModel
+        .loginUser(userData)
+        .then(response => done(new Error()),
+        reject => done());
+    });
+
+    it('Login with invalid creadentials should cause error', () => {
+      let userData = {};
+
+      app.userModel
+        .loginUser(userData)
+        .then(response => done(new Error()),
+        reject => done());
     });
   });
 });
